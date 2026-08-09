cb4fafae56ccccf20db252bc46ba09920681a2f5
Gaurav Mittal <gauravmittalsn@gmail.com>
2026-08-09 16:11:12 +0530

    backend add

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.79.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "sg"
+    storage_account_name = "gm1" 
+    container_name = "gm12"
+    key = "gm.terrform.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
