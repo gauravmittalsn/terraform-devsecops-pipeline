56f7074775a35696a5a0b6c1e2ec75357acaa8ef
Gaurav Mittal <gauravmittalsn@gmail.com>
2026-08-09 16:28:33 +0530

    backend remove

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "4.79.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "sg"
-    storage_account_name = "gm1" 
-    container_name = "gm12"
-    key = "gm.terrform.tfstate"
-  }
 }
 provider "azurerm" {
   features {}
